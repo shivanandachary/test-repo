4907efe2a9c4192155563717c18600b769a68390
Shivananda <shivananda0011@gmail.com>
2023-12-17 16:28:08 +0530

    spring-data-jpa-postress-demo skeleton project

---
+++ spring-data-jpa-postgres/src/main/java/com/spring/data/jpa/demp/springdatajpapostgres/SpringDataJpaPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.spring.data.jpa.demp.springdatajpapostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaPostgresApplication.class, args);
+	}
+
+}
---
+++ spring-data-jpa-postgres/src/test/java/com/spring/data/jpa/demp/springdatajpapostgres/SpringDataJpaPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.data.jpa.demp.springdatajpapostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
